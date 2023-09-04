0327fca3f5f14639967f5c5fab088e235b3c286c
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2023-09-05 00:24:31 +0200

    add: bump patch version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExRabbitMQAdmin.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @description "A Http client library for RabbitMQ Web API"
   @source_url "https://github.com/Intility/ex_rabbitmq_admin"
 
