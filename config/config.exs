db278852d11813ff1c13bd924642c910a5aff0af
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2024-12-17 15:23:57 +0100

    fix: default config

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ case config_env() do
   :dev ->
     config :tesla, adapter: {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
 
-    config :ex_rabbitmq_admin, ExRabbitMQAdmin.Client,
+    config :ex_rabbitmq_admin, ExRabbitMQAdmin,
       base_url: System.get_env("RABBITMQ_HTTP_BASE_URL", "http://localhost:15672"),
       username: "guest",
       password: "guest"
